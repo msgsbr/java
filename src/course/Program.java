7589b50ce8e3181fd5e9020708c95a0684779f77
msgs <msgs@192.168.0.104>
2018-10-17 23:04:56 -0300

    second commit

--- src/course/Program.java
+++ src/course/Program.java
@@ -5,6 +5,13 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("Saida de Dados!");
+		
+		int n = 5;
+		int y = 2;
+		
+		float z = (float) n / y;
+		
+		System.out.println(z);
 
 	}
 
