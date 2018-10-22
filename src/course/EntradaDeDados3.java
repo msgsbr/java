1bcfc366db398b3cf5237cfdb403392a4bc0690c
msgs <msgs@192.168.0.104>
2018-10-22 00:08:43 -0300

    atualizacao curso

---
+++ src/course/EntradaDeDados3.java
@@ -0,0 +1,37 @@
+package course;
+
+import java.util.Scanner;
+
+public class EntradaDeDados3 {
+
+	public static void main(String[] args) {
+		
+		Scanner data = new Scanner(System.in);
+		
+		// insira nome completo
+		String nomeCompleto = data.nextLine();
+		// insira quantos quartos em sua residencia
+		int quartos = data.nextInt();
+		// insira o preco do produto
+		double preco = data.nextDouble();
+		// insira seu sobrenome, idade e altura na mesma linha
+		String sobreNome = data.next();
+		int idade = data.nextInt();
+		double altura = data.nextDouble();
+		
+		System.out.println(nomeCompleto);
+		System.out.println(quartos);
+		System.out.println(preco);
+		
+		System.out.println(sobreNome);
+		System.out.println(idade);
+		System.out.println(altura);
+		
+		
+		// FINALIZANDO O USO DA CLASSE
+		data.close();
+		
+
+	}
+
+}
--- src/course/EntredaDeDados2.java
+++ src/course/EntredaDeDados2.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class EntredaDeDados2 {
 
 	public static void main(String[] args) {
-		/*
-		 * 
-		 */
+		
+		// Para mudar o formato da moeda de ',' para '.' é necessario alterar o IDIOMA/LOCALIZACAO.
+		// IMPORTANTE: a alteracao de idioma deve ser realizada antes de instanciar a classe Scanner de entrada de dados
+		//Locale.setDefault(Locale.US);
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int n1 = sc.nextInt();
@@ -20,16 +22,30 @@ public class EntredaDeDados2 {
 		String s = sc.next(); // b5
 		char letter = s.charAt(0); // pega o primeiro caracter
 		int digit = Integer.parseInt( s.substring(1) ); // subtrai o primeiro caracter, necessario 'parseInt' para transformar String em Inteiro
+
+		// 4.32
+		double n2 = sc.nextDouble();
 		
-		// 4.23
-		
-		
+		// Marcelo M 39 1.80, guardando em variaveis separadas
+		String name2 = sc.next();
+		char ch = sc.next().charAt(0);
+		int age = sc.nextInt();
+		double height = sc.nextDouble();
+
 		System.out.println(n1);
 		System.out.println(name);
 		System.out.println(gender);
 		System.out.println(letter);
 		System.out.println(digit);
 		
+		System.out.println(n2);
+		
+		System.out.println(name2);
+		System.out.println(ch);
+		System.out.println(age);
+		System.out.println(height);
+		
+		// fecha a classe depois do uso
 		sc.close();
 	}
 
