ac5bda2013f09ba5435ab1ae7feb4cdad9cfd0c1
msgs <msgs@192.168.0.104>
2019-01-15 02:32:24 -0200

    Atualizacao de cod. para calcular a area de um triangulo (OOP)

--- src/course/AreaTrianguloOOP.java
+++ src/course/AreaTrianguloOOP.java
@@ -23,11 +23,8 @@ public class AreaTrianguloOOP {
 		y.b = data.nextDouble();
 		y.c = data.nextDouble();
 		
-		double p = (x.a + x.b + x.c) / 2.0; 
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-		
-		p = (y.a + y.b + y.c) / 2.0; 
-		double areaY = Math.sqrt(p * (p - y.a) * (p-y.b) * (p-y.c));
+		double areaX = x.area();
+		double areaY = y.area();
 		
 		System.out.printf("Triangle X area: %.3f%n", areaX);
 		System.out.printf("Triangle Y area: %.3f%n", areaY);
--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -9,5 +9,10 @@ public class Triangle {
 	public static void main(String[] args) {	
 		
 	}
+	
+	public double area() {
+		double p = (a + b + c) / 2;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
 
 }
